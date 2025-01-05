f12f3b8858b695e194f0705f4838f429f4abd9d1
qpeyba <145938039+qpeyba@users.noreply.github.com>
2025-01-05 05:32:09 +0300

    todo: Update remaining tests to use new library for ObjReader

--- tests/com/cgvsu/obj_io/writer/ObjReaderTest.java
+++ tests/com/cgvsu/obj_io/writer/ObjReaderTest.java
@@ -22,50 +22,51 @@ class ObjReaderTest {
         Assertions.assertTrue(result.equals(expectedResult));
     }
 
-    // @Test
-    // public void testParseVertex02() {
-    //     ArrayList<String> wordsInLineWithoutToken = new ArrayList<>(Arrays.asList("1.01", "1.02", "1.03"));
-    //     Vector3f result = ObjReader.parseVertex(wordsInLineWithoutToken, 5);
-    //     Vector3f expectedResult = new Vector3f(1.01f, 1.02f, 1.10f);
-    //     Assertions.assertFalse(result.equals(expectedResult));
-    // }
+    // todo: Update remaining tests to use new library for ObjReader
+/*    @Test
+    public void testParseVertex02() {
+        ArrayList<String> wordsInLineWithoutToken = new ArrayList<>(Arrays.asList("1.01", "1.02", "1.03"));
+        Vector3f result = ObjReader.parseVertex(wordsInLineWithoutToken, 5);
+        Vector3f expectedResult = new Vector3f(1.01f, 1.02f, 1.10f);
+        Assertions.assertFalse(result.equals(expectedResult));
+    }
 
-    // @Test
-    // public void testParseVertex03() {
-    //     ArrayList<String> wordsInLineWithoutToken = new ArrayList<>(Arrays.asList("ab", "o", "ba"));
-    //     try {
-    //         ObjReader.parseVertex(wordsInLineWithoutToken, 10);
-    //     } catch (ObjReaderException exception) {
-    //         String expectedError = "Error parsing OBJ file on line: 10. Failed to parse float value.";
-    //         Assertions.assertEquals(expectedError, exception.getMessage());
-    //     }
-    // }
+    @Test
+    public void testParseVertex03() {
+        ArrayList<String> wordsInLineWithoutToken = new ArrayList<>(Arrays.asList("ab", "o", "ba"));
+        try {
+            ObjReader.parseVertex(wordsInLineWithoutToken, 10);
+        } catch (ObjReaderException exception) {
+            String expectedError = "Error parsing OBJ file on line: 10. Failed to parse float value.";
+            Assertions.assertEquals(expectedError, exception.getMessage());
+        }
+    }
 
-    // @Test
-    // public void testParseVertex04() {
-    //     ArrayList<String> wordsInLineWithoutToken = new ArrayList<>(Arrays.asList("1.0", "2.0"));
-    //     try {
-    //         ObjReader.parseVertex(wordsInLineWithoutToken, 10);
-    //     } catch (ObjReaderException exception) {
-    //         String expectedError = "Error parsing OBJ file on line: 10. Too few vertex arguments.";
-    //         Assertions.assertEquals(expectedError, exception.getMessage());
-    //     }
-    // }
+    @Test
+    public void testParseVertex04() {
+        ArrayList<String> wordsInLineWithoutToken = new ArrayList<>(Arrays.asList("1.0", "2.0"));
+        try {
+            ObjReader.parseVertex(wordsInLineWithoutToken, 10);
+        } catch (ObjReaderException exception) {
+            String expectedError = "Error parsing OBJ file on line: 10. Too few vertex arguments.";
+            Assertions.assertEquals(expectedError, exception.getMessage());
+        }
+    }
 
-    // @Test
-    // public void testParseVertex05() {
-    //     // АГААА! Вот тест, который говорит, что у метода нет проверки на более, чем 3 числа
-    //     // А такой случай лучше не игнорировать, а сообщать пользователю, что у него что-то не так
-    //     // ассерт, чтобы не забыть про тест:
-    //     Assertions.assertTrue(false);
+    @Test
+    public void testParseVertex05() {
+        // АГААА! Вот тест, который говорит, что у метода нет проверки на более, чем 3 числа
+        // А такой случай лучше не игнорировать, а сообщать пользователю, что у него что-то не так
+        // ассерт, чтобы не забыть про тест:
+        Assertions.assertTrue(false);
 
 
-    //     ArrayList<String> wordsInLineWithoutToken = new ArrayList<>(Arrays.asList("1.0", "2.0", "3.0", "4.0"));
-    //     try {
-    //         ObjReader.parseVertex(wordsInLineWithoutToken, 10);
-    //     } catch (ObjReaderException exception) {
-    //         String expectedError = "";
-    //         Assertions.assertEquals(expectedError, exception.getMessage());
-    //     }
-    // }
+        ArrayList<String> wordsInLineWithoutToken = new ArrayList<>(Arrays.asList("1.0", "2.0", "3.0", "4.0"));
+        try {
+            ObjReader.parseVertex(wordsInLineWithoutToken, 10);
+        } catch (ObjReaderException exception) {
+            String expectedError = "";
+            Assertions.assertEquals(expectedError, exception.getMessage());
+        }
+    }*/
 }
