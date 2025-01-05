7287ce50f6bf095a11b364b2342362fb6b5d90c7
Artem Sumin <qpeyba@ya.ru>
2025-01-05 05:26:10 +0300

    Partially update ObjReaderTest class to use new library for ObjReader
    and migrate tests to new package structure

---
+++ tests/com/cgvsu/obj_io/writer/ObjReaderTest.java
@@ -0,0 +1,71 @@
+package com.cgvsu.obj_io.writer;
+
+import com.cgvsu.math.Vector3f;
+import com.cgvsu.model.Model;
+import com.cgvsu.obj_io.reader.ObjReader;
+import com.cgvsu.obj_io.reader.ObjReaderException;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class ObjReaderTest {
+
+    @Test
+    public void testParseVertex01() {
+        String objVer = "v 1.01 1.02 1.03";
+        Model model = ObjReader.read(objVer);
+        Vector3f result = model.vertices.get(0);
+        Vector3f expectedResult = new Vector3f(1.01f, 1.02f, 1.03f);
+        Assertions.assertTrue(result.equals(expectedResult));
+    }
+
+    // @Test
+    // public void testParseVertex02() {
+    //     ArrayList<String> wordsInLineWithoutToken = new ArrayList<>(Arrays.asList("1.01", "1.02", "1.03"));
+    //     Vector3f result = ObjReader.parseVertex(wordsInLineWithoutToken, 5);
+    //     Vector3f expectedResult = new Vector3f(1.01f, 1.02f, 1.10f);
+    //     Assertions.assertFalse(result.equals(expectedResult));
+    // }
+
+    // @Test
+    // public void testParseVertex03() {
+    //     ArrayList<String> wordsInLineWithoutToken = new ArrayList<>(Arrays.asList("ab", "o", "ba"));
+    //     try {
+    //         ObjReader.parseVertex(wordsInLineWithoutToken, 10);
+    //     } catch (ObjReaderException exception) {
+    //         String expectedError = "Error parsing OBJ file on line: 10. Failed to parse float value.";
+    //         Assertions.assertEquals(expectedError, exception.getMessage());
+    //     }
+    // }
+
+    // @Test
+    // public void testParseVertex04() {
+    //     ArrayList<String> wordsInLineWithoutToken = new ArrayList<>(Arrays.asList("1.0", "2.0"));
+    //     try {
+    //         ObjReader.parseVertex(wordsInLineWithoutToken, 10);
+    //     } catch (ObjReaderException exception) {
+    //         String expectedError = "Error parsing OBJ file on line: 10. Too few vertex arguments.";
+    //         Assertions.assertEquals(expectedError, exception.getMessage());
+    //     }
+    // }
+
+    // @Test
+    // public void testParseVertex05() {
+    //     // АГААА! Вот тест, который говорит, что у метода нет проверки на более, чем 3 числа
+    //     // А такой случай лучше не игнорировать, а сообщать пользователю, что у него что-то не так
+    //     // ассерт, чтобы не забыть про тест:
+    //     Assertions.assertTrue(false);
+
+
+    //     ArrayList<String> wordsInLineWithoutToken = new ArrayList<>(Arrays.asList("1.0", "2.0", "3.0", "4.0"));
+    //     try {
+    //         ObjReader.parseVertex(wordsInLineWithoutToken, 10);
+    //     } catch (ObjReaderException exception) {
+    //         String expectedError = "";
+    //         Assertions.assertEquals(expectedError, exception.getMessage());
+    //     }
+    // }
+}
