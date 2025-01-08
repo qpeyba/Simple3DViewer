0e96341097a464737f8eb6625a2ca5318707d03d
KoStickSipliy <esipov.kostya.20005@gmail.com>
2025-01-09 00:28:15 +0300

    Added some small tests

---
+++ tests/com/cgvsu/obj_io/writer/GraphicConveyorTest.java
@@ -0,0 +1,10 @@
+package com.cgvsu.obj_io.writer;
+
+import com.cgvsu.render_engine.Camera;
+import io.github.alphameo.linear_algebra.vec.*;
+import io.github.alphameo.linear_algebra.vec.Vector3;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+public class GraphicConveyorTest {
+
+}
