b31dd29517a13b388410cf4833e78af7537ac917
KoStickSipliy <esipov.kostya.20005@gmail.com>
2025-01-08 20:53:06 +0300

    Every Matrix (lookAt and Perspective) is correct now

--- src/main/java/com/cgvsu/render_engine/GraphicConveyor.java
+++ src/main/java/com/cgvsu/render_engine/GraphicConveyor.java
@@ -2,6 +2,8 @@ package com.cgvsu.render_engine;
 //import javax.vecmath.*;
 import io.github.alphameo.linear_algebra.mat.*;
 import io.github.alphameo.linear_algebra.vec.*;
+import static io.github.alphameo.linear_algebra.mat.Matrix4Col.*;
+import static io.github.alphameo.linear_algebra.mat.Matrix4Row.*;
 
 public class GraphicConveyor { // not to be used
 
@@ -18,27 +20,21 @@ public class GraphicConveyor { // not to be used
         return lookAt(eye, target, new Vec3(0F, 1.0F, 0F));
     }
 
-    //ВОЗМОЖНА ПРОБЛЕМА
     public static Matrix4 lookAt(Vector3 eye, Vector3 target, Vector3 up) {
-        Vector3 resultX = new Vec3();
-        Vector3 resultY = new Vec3();
-        Vector3 resultZ = new Vec3();
-
-        resultZ = Vec3Math.sub(target, eye);
-        resultX = Vec3Math.cross(up, resultZ);
-        resultY = Vec3Math.cross(resultZ, resultX);
+        Vector3 resultZ = Vec3Math.subtracted(target, eye);
+        Vector3 resultX = Vec3Math.cross(up, resultZ);
+        Vector3 resultY = Vec3Math.cross(resultZ, resultX);
 
         Vec3Math.normalize(resultX);
         Vec3Math.normalize(resultY);
         Vec3Math.normalize(resultZ);
 
-        float[][] matrix = new float[][]{
-                {resultX.x(), resultY.x(), resultZ.x(), 0},
-                {resultX.y(), resultY.y(), resultZ.y(), 0},
-                {resultX.z(), resultY.z(), resultZ.z(), 0},
-                {-(Vec3Math.dot(resultX, eye)), -(Vec3Math.dot(resultY, eye)), -(Vec3Math.dot(resultZ, eye)), 1}}; // ?
-//                -resultX.dot(eye), -resultY.dot(eye), -resultZ.dot(eye), 1}; -- старый вариант
-        return new Mat4 (matrix);
+        return new Mat4(
+                resultX.x(), resultX.y(), resultX.z(), -Vec3Math.dot(resultX, eye),
+                resultY.x(), resultY.y(), resultY.z(), -Vec3Math.dot(resultY, eye),
+                resultZ.x(), resultZ.y(), resultZ.z(), -Vec3Math.dot(resultZ, eye),
+                0, 0, 0, 1
+        );
     }
 
     public static Matrix4 perspective(
@@ -48,21 +44,14 @@ public class GraphicConveyor { // not to be used
             final float farPlane) {
         Matrix4 result = new Mat4();
         float tangentMinusOnDegree = (float) (1.0F / (Math.tan(fov * 0.5F)));
-        result.set(0, 0, tangentMinusOnDegree / aspectRatio);
-        result.set(1, 1, tangentMinusOnDegree);
-        result.set(2, 2, (farPlane + nearPlane) / (farPlane - nearPlane));
-        result.set(2, 3, 1.0F);
-        result.set(3, 2, 2 * (nearPlane * farPlane) / (nearPlane - farPlane));
-
-//        result.m00 = tangentMinusOnDegree / aspectRatio;
-//        result.m11 = tangentMinusOnDegree;
-//        result.m22 = (farPlane + nearPlane) / (farPlane - nearPlane);
-//        result.m23 = 1.0F;
-//        result.m32 = 2 * (nearPlane * farPlane) / (nearPlane - farPlane);
+        result.set(R0, C0, tangentMinusOnDegree);
+        result.set(R1, C1, tangentMinusOnDegree / aspectRatio);
+        result.set(R2, C2, (farPlane + nearPlane) / (farPlane - nearPlane));
+        result.set(R3, C2, 1.0F);
+        result.set(R2, C3, 2 * (nearPlane * farPlane) / (nearPlane - farPlane));
         return result;
     }
 
-    // ВОЗМОЖНА ПРОБЛЕМА
 //    public static Vec3 multiplyMatrix4ByVector3(final Matrix4 matrix, final Vector3 vertex) {
 //        Mat4Math.transpose(matrix); //?
 //        final float x = (vertex.x() * matrix.get(0, 0)) + (vertex.y() * matrix.get(1, 0)) + (vertex.z() * matrix.get(2, 0)) + matrix.get(3, 0);
