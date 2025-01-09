5dd1210cdde3c5a686bff5cb790adeb37ce3d660
qpeyba <145938039+qpeyba@users.noreply.github.com>
2025-01-09 21:48:46 +0300

    Fixing branch issue

--- src/main/java/com/cgvsu/GuiController.java
+++ src/main/java/com/cgvsu/GuiController.java
@@ -1,6 +1,8 @@
 package com.cgvsu;
 
 import com.cgvsu.render_engine.RenderEngine;
+import io.github.alphameo.linear_algebra.vec.Vec3;
+import io.github.alphameo.linear_algebra.vec.Vector3;
 import javafx.fxml.FXML;
 import javafx.animation.Animation;
 import javafx.animation.KeyFrame;
@@ -43,8 +45,8 @@ public class GuiController {
     private Model mesh = null;
 
     private Camera camera = new Camera(
-            new Vector3f(0, 00, 100),
-            new Vector3f(0, 0, 0),
+            new Vec3(0, 00, 100),
+            new Vec3(0, 0, 0),
             1.0F, 1, 0.01F, 100);
 
     private Timeline timeline;
@@ -96,35 +98,48 @@ public class GuiController {
 
         }
     }
+    @FXML
+    private TextField inputForModelMoving;
+
+    @FXML
+    private TextField inputForModelRotation;
+
+    @FXML
+    private TextField inputForModelScaling;
+
+    @FXML
+    void applyTransformation(MouseEvent event) {
+
+    }
 
     @FXML
     public void handleCameraForward(ActionEvent actionEvent) {
-        camera.movePosition(new Vector3f(0, 0, -TRANSLATION));
+        camera.movePosition(new Vec3(0, 0, -TRANSLATION));
     }
 
     @FXML
     public void handleCameraBackward(ActionEvent actionEvent) {
-        camera.movePosition(new Vector3f(0, 0, TRANSLATION));
+        camera.movePosition(new Vec3(0, 0, TRANSLATION));
     }
 
     @FXML
     public void handleCameraLeft(ActionEvent actionEvent) {
-        camera.movePosition(new Vector3f(TRANSLATION, 0, 0));
+        camera.movePosition(new Vec3(TRANSLATION, 0, 0));
     }
 
     @FXML
     public void handleCameraRight(ActionEvent actionEvent) {
-        camera.movePosition(new Vector3f(-TRANSLATION, 0, 0));
+        camera.movePosition(new Vec3(-TRANSLATION, 0, 0));
     }
 
     @FXML
     public void handleCameraUp(ActionEvent actionEvent) {
-        camera.movePosition(new Vector3f(0, TRANSLATION, 0));
+        camera.movePosition(new Vec3(0, TRANSLATION, 0));
     }
 
     @FXML
     public void handleCameraDown(ActionEvent actionEvent) {
-        camera.movePosition(new Vector3f(0, -TRANSLATION, 0));
+        camera.movePosition(new Vec3(0, -TRANSLATION, 0));
     }
 
     @FXML
@@ -147,7 +162,7 @@ public class GuiController {
             System.out.println("Error saving file: " + e.getMessage());
         }
     }
-  
+
     @FXML
     private TextField VerticesToRemove;
     @FXML
@@ -169,7 +184,7 @@ public class GuiController {
 
         try {
             List<Integer> verticesToRemove = parseVerticesToRemove(VerticesToRemove.getText());
-          
+
             if (!validateVertexIndices(verticesToRemove)) {
                 showError("Invalid vertices", "One or more vertex indices are out of range.");
                 return;
--- src/main/java/com/cgvsu/render_engine/RenderEngine.java
+++ src/main/java/com/cgvsu/render_engine/RenderEngine.java
@@ -1,11 +1,21 @@
 package com.cgvsu.render_engine;
 
+import java.awt.datatransfer.Transferable;
 import java.util.ArrayList;
 
+//import com.cgvsu.math.Vector3f;
+//import javafx.scene.canvas.GraphicsContext;
 import com.cgvsu.math.Vector3f;
-import javafx.scene.canvas.GraphicsContext;
-import javax.vecmath.*;
+import com.cgvsu.math.affine.AffineTransformation;
+import com.cgvsu.math.affine.Transformation;
+import io.github.alphameo.linear_algebra.vec.*;
+import io.github.alphameo.linear_algebra.mat.*;
 import com.cgvsu.model.Model;
+import javafx.scene.canvas.GraphicsContext;
+
+import javax.vecmath.Matrix4f;
+import javax.vecmath.Point2f;
+
 import static com.cgvsu.render_engine.GraphicConveyor.*;
 
 public class RenderEngine {
@@ -17,42 +27,75 @@ public class RenderEngine {
             final int width,
             final int height)
     {
-        Matrix4f modelMatrix = rotateScaleTranslate();
-        Matrix4f viewMatrix = camera.getViewMatrix();
-        Matrix4f projectionMatrix = camera.getProjectionMatrix();
+        //Matrix4 modelMatrix = new Transformation().getMatrix();
+        Matrix4 modelMatrix = rotateScaleTranslate();
+        Matrix4 viewMatrix = camera.getViewMatrix();
+        Matrix4 projectionMatrix = camera.getProjectionMatrix();
 
-        Matrix4f modelViewProjectionMatrix = new Matrix4f(modelMatrix);
-        modelViewProjectionMatrix.mul(viewMatrix);
-        modelViewProjectionMatrix.mul(projectionMatrix);
+        Matrix4 modelViewProjectionMatrix = new Mat4(projectionMatrix);
+        modelViewProjectionMatrix = Mat4Math.prod(modelViewProjectionMatrix, viewMatrix);
+        modelViewProjectionMatrix = Mat4Math.prod(modelViewProjectionMatrix, modelMatrix);
+//        modelViewProjectionMatrix.mul(viewMatrix);
+//        modelViewProjectionMatrix.mul(projectionMatrix);
 
         final int nPolygons = mesh.polygons.size();
         for (int polygonInd = 0; polygonInd < nPolygons; ++polygonInd) {
             final int nVerticesInPolygon = mesh.polygons.get(polygonInd).getVertexIndices().size();
 
-            ArrayList<Point2f> resultPoints = new ArrayList<>();
+            ArrayList<Vector2> resultPoints = new ArrayList<>();
             for (int vertexInPolygonInd = 0; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
-                Vector3f vertex = mesh.vertices.get(mesh.polygons.get(polygonInd).getVertexIndices().get(vertexInPolygonInd));
+                Vector3 vertex = mesh.vertices.get(mesh.polygons.get(polygonInd).getVertexIndices().get(vertexInPolygonInd));
 
-                javax.vecmath.Vector3f vertexVecmath = new javax.vecmath.Vector3f(vertex.getX(), vertex.getY(), vertex.getZ());
+                Vector3 vertexVecmath = new Vec3(vertex.x(), vertex.y(), vertex.z());
 
-                Point2f resultPoint = vertexToPoint(multiplyMatrix4ByVector3(modelViewProjectionMatrix, vertexVecmath), width, height);
+                Vector2 resultPoint = vertexToVec2(multiplyMatrix4ByVector3(modelViewProjectionMatrix, vertexVecmath), width, height);
                 resultPoints.add(resultPoint);
             }
 
             for (int vertexInPolygonInd = 1; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
                 graphicsContext.strokeLine(
-                        resultPoints.get(vertexInPolygonInd - 1).x,
-                        resultPoints.get(vertexInPolygonInd - 1).y,
-                        resultPoints.get(vertexInPolygonInd).x,
-                        resultPoints.get(vertexInPolygonInd).y);
+                        resultPoints.get(vertexInPolygonInd - 1).x(),
+                        resultPoints.get(vertexInPolygonInd - 1).y(),
+                        resultPoints.get(vertexInPolygonInd).x(),
+                        resultPoints.get(vertexInPolygonInd).y());
             }
 
             if (nVerticesInPolygon > 0)
                 graphicsContext.strokeLine(
-                        resultPoints.get(nVerticesInPolygon - 1).x,
-                        resultPoints.get(nVerticesInPolygon - 1).y,
-                        resultPoints.get(0).x,
-                        resultPoints.get(0).y);
+                        resultPoints.get(nVerticesInPolygon - 1).x(),
+                        resultPoints.get(nVerticesInPolygon - 1).y(),
+                        resultPoints.get(0).x(),
+                        resultPoints.get(0).y());
         }
+
+//        final int nPolygons = mesh.polygons.size();
+//        for (int polygonInd = 0; polygonInd < nPolygons; ++polygonInd) {
+//            final int nVerticesInPolygon = mesh.polygons.get(polygonInd).getVertexIndices().size();
+//
+//            ArrayList<Vector2> resultPoints = new ArrayList<>();
+//            for (int vertexInPolygonInd = 0; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
+//                Vector3 vertex = mesh.vertices.get(mesh.polygons.get(polygonInd).getVertexIndices().get(vertexInPolygonInd));
+//
+//                Vector3 vertexVecmath = new Vec3(vertex.x(), vertex.y(), vertex.y());
+//
+//                Vector2 resultPoint = vertexToPoint(multiplyMatrix4ByVector3(modelViewProjectionMatrix, vertexVecmath), width, height);
+//                resultPoints.add(resultPoint);
+//            }
+//
+//            for (int vertexInPolygonInd = 1; vertexInPolygonInd < nVerticesInPolygon; ++vertexInPolygonInd) {
+//                graphicsContext.strokeLine(
+//                        resultPoints.get(vertexInPolygonInd - 1).x(),
+//                        resultPoints.get(vertexInPolygonInd - 1).y(),
+//                        resultPoints.get(vertexInPolygonInd).x(),
+//                        resultPoints.get(vertexInPolygonInd).y());
+//            }
+//
+//            if (nVerticesInPolygon > 0)
+//                graphicsContext.strokeLine(
+//                        resultPoints.get(nVerticesInPolygon - 1).x(),
+//                        resultPoints.get(nVerticesInPolygon - 1).y(),
+//                        resultPoints.get(0).x(),
+//                        resultPoints.get(0).y());
+//        }
     }
 }
